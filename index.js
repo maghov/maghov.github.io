f703ad8c87a34456852f74162d9b2ee34e6d46f4
Magnus <maghovk@gmail.com>
2023-04-07 14:10:04 +0200

    Update and rename idex.js to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyBIy7mb_wS05I_0ICLMSX1P5avFim7yXsA",
+    authDomain: "chat-gtp-b0e1d.firebaseapp.com",
+    projectId: "chat-gtp-b0e1d",
+    storageBucket: "chat-gtp-b0e1d.appspot.com",
+    messagingSenderId: "617084173940",
+    appId: "1:617084173940:web:e66612d6dae0337d4e9b21",
+    measurementId: "G-TFJ234R5B9",
+    databaseURL:"https://chat-gtp-b0e1d-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+  
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+
+// Add data to a Firebase Realtime Database location
+database.ref('users').push({
+    name: "John Doe",
+    age: 25,
+    email: "johndoe@example.com"
+}, function(error) {
+    if (error) {
+        console.log("Error adding data: ", error);
+    } else {
+        console.log("Data added successfully!");
+    }
+});
+
