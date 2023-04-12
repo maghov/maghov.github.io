f2c117dc2282c760437370bf71dae7017330d38a
Magnus <maghovk@gmail.com>
2023-04-12 19:27:01 +0200

    12

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
 
-//import { initializeApp } from "firebase/app";
-//import { getDatabase } from "firebase/database";
-
-
-
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
-import { collection, getDocs, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
-import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-console.log('index js');
-
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
  const firebaseConfig = {
-    apiKey: "AIzaSyBIy7mb_wS05I_0ICLMSX1P5avFim7yXsA",
-    authDomain: "chat-gtp-b0e1d.firebaseapp.com",
-    projectId: "chat-gtp-b0e1d",
-    storageBucket: "chat-gtp-b0e1d.appspot.com",
-    messagingSenderId: "617084173940",
-    appId: "1:617084173940:web:e66612d6dae0337d4e9b21",
-    measurementId: "G-TFJ234R5B9",
-    databaseURL:"https://chat-gtp-b0e1d-default-rtdb.europe-west1.firebasedatabase.app/"
-  };
-  
-const app = initializeApp(firebaseConfig);
+   apiKey: "AIzaSyBIy7mb_wS05I_0ICLMSX1P5avFim7yXsA",
+   authDomain: "chat-gtp-b0e1d.firebaseapp.com",
+   databaseURL: "https://chat-gtp-b0e1d-default-rtdb.europe-west1.firebasedatabase.app",
+   projectId: "chat-gtp-b0e1d",
+   storageBucket: "chat-gtp-b0e1d.appspot.com",
+   messagingSenderId: "617084173940",
+   appId: "1:617084173940:web:e66612d6dae0337d4e9b21",
+   measurementId: "G-TFJ234R5B9"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
+
+ import {getDatabase, ref, set, child, update, remove, get, onValue}
+ from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js"
+
+ const db = getDatabase();
 
 
+ var namebox = document.getElementById("name")
+ var numberbox = document.getElementById("number")
+ //var datebox = document.getElementById("inputDate")
+ var datebox = document.getElementById("inputDate");
+ var toDay = new Date();
 
-// Initialize Realtime Database and get a reference to the service
-const database = getFirestore(app);
 
+ console.log('toDay' + toDay)
+ console.log('datebox value' + datebox)
+ var insBtn = document.getElementById("addBtn")
 
-// Add data to a Firebase Realtime Database location
-database.ref('users').push({
-    name: "John Doe",
-    age: 25,
-    email: "johndoe@example.com"
-}, function(error) {
-    if (error) {
-        console.log("Error adding data: ", error);
-    } else {
-        console.log("Data added successfully!");
-    }
+ function InsertDate(){
+     set(ref(db, "TheStudents/"+  namebox.value),{
+     NameofStd: namebox.value, 
+     NumberofStd: numberbox.value, 
+     Date: datebox.value,
+ })
+.then(() => {
+console.log("Data saved successfully!")
+})
+.catch((error) => {
+console.log("Error!")
 });
+ }
 
+
+ const dbRef = ref(db, '/a/b/c');
+
+onValue(dbRef, (snapshot) => {
+snapshot.forEach((childSnapshot) => {
+const childKey = childSnapshot.key;
+const childData = childSnapshot.val();
+console.log(childData)
+});
+}, {
+onlyOnce: true
+});
