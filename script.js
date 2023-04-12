c4ddd95ead022f1d80a9bc4a99784907ad02ae8c
Magnus <maghovk@gmail.com>
2023-04-12 19:14:40 +0200

    12

--- script.js
+++ script.js
@@ -8,18 +8,7 @@ console.log('scrip.js is running')
 
 ////////////////////////////
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyBIy7mb_wS05I_0ICLMSX1P5avFim7yXsA",
-   authDomain: "chat-gtp-b0e1d.firebaseapp.com",
-   databaseURL: "https://chat-gtp-b0e1d-default-rtdb.europe-west1.firebasedatabase.app",
-   projectId: "chat-gtp-b0e1d",
-   storageBucket: "chat-gtp-b0e1d.appspot.com",
-   messagingSenderId: "617084173940",
-   appId: "1:617084173940:web:e66612d6dae0337d4e9b21",
-   measurementId: "G-TFJ234R5B9"
- };
+
 
 
 
