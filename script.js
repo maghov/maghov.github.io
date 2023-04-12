b8153cfa1e52f9aa0169b9c242cfad6aee805e72
Magnus <maghovk@gmail.com>
2023-04-12 19:10:13 +0200

    12

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ const addedDates = [];
 console.log('scrip.js is running')
 
 ////////////////////////////
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
 
  // Your web app's Firebase configuration
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -30,8 +25,7 @@ console.log('scrip.js is running')
  const app = initializeApp(firebaseConfig);
  const analytics = getAnalytics(app);
 
- import {getDatabase, ref, set, child, update, remove, get, onValue}
- from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js"
+
 
  const db = getDatabase();
 
