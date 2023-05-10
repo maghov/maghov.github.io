9d657ad8dd04995dc0d5fa9f1fca3fe35e0aad51
Magnus <maghovk@gmail.com>
2023-05-10 22:23:32 +0200

    12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+ /*import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
@@ -64,3 +64,6 @@ console.log(childData)
 }, {
 onlyOnce: true
 });
+*/
+
+var newNumber = '12'
